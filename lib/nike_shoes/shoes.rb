45b9c00d68e86d127ddc82d0df0fe109e4d31261
Daksha Patel <dakshapatel100@gmail.com>
2018-06-22 21:35:03 +0000

    Add scrape data

--- lib/nike_shoes/shoes.rb
+++ lib/nike_shoes/shoes.rb
@@ -9,10 +9,16 @@ end
 def self.scrape_shoes
 
 doc = Nokogiri::HTML(open('https://store.nike.com/us/en_us/pw/mens-shoes/7puZoi3'))
-doc.css('.product-display-name').each do |shoe_name|
-  name = shoe_name.text
-binding.pry
 
+doc.css('.grid-item-info').each do |shoe|
+  name = shoe.text
+
+
+shoe.css('.product-name').text.split("\n").map(&:strip).select do |shoe_info|
+ shoe_info.length > 0
+    
+  end
+binding.pry
 end
 end
 
