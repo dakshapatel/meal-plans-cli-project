89bdb46d039e4661cb1e2886e84b5ac04550c6cf
Daksha Patel <dakshapatel100@gmail.com>
2018-06-22 21:55:59 +0000

    Add scrape data for price

--- lib/nike_shoes/shoes.rb
+++ lib/nike_shoes/shoes.rb
@@ -2,24 +2,26 @@ require "pry"
 class NikeShoes::Shoes
   attr_accessor :name,
 
-def self.all
+  def self.all
 
-end
-
-def self.scrape_shoes
-
-doc = Nokogiri::HTML(open('https://store.nike.com/us/en_us/pw/mens-shoes/7puZoi3'))
-
-doc.css('.grid-item-info').each do |shoe|
-  name = shoe.text
-
-
-shoe.css('.product-name').text.split("\n").map(&:strip).select do |shoe_info|
- shoe_info.length > 0
-    
   end
-binding.pry
-end
-end
+
+  def self.scrape_shoes
+
+    doc = Nokogiri::HTML(open('https://store.nike.com/us/en_us/pw/mens-shoes/7puZoi3'))
+
+    price = doc.css('.prices').text.split("\n").map(&:strip).select do |shoe_price|
+      shoe_price.length > 0
+    end
+
+
+    name = doc.css('.product-name').text.split("\n").map(&:strip).select do |shoe_info|
+     shoe_info.length > 0
+    end
+  binding.pry
+  end
+
+
+
 
 end
