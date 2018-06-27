d2bb78d601bbdb4e7d41f88ad43496e82823ca9f
Daksha Patel <dakshapatel100@gmail.com>
2018-06-27 22:18:39 +0000

    Save Progress

--- lib/nike_shoes/cli.rb
+++ lib/nike_shoes/cli.rb
@@ -4,11 +4,8 @@ class NikeShoes::CLI
 
 
   def call
-
     list_gender
     start
-    NikeShoes::Shoes.scrape_shoes
-
   end
 
   def list_gender
@@ -20,20 +17,23 @@ class NikeShoes::CLI
     3. Big Kids'
     4. Little Kids'
     DOC
+    @shoes = NikeShoes::Shoes.get_shoe_info
+    @shoes.each.with_index(1) do |shoes, i|
+      puts "#{i}. #{shoe.name} - #{shoe.price}."
+    end
   end
-end
 
   def start
-
     input = "nil"
     while input != "exit"
-      puts "Enter the number to see shoes in your gender or type exit"
+      puts "Enter the number associated with the gender or type exit"
       input = gets.strip
 
       case input
         when "1"
-          NikeShoes::Shoes.get_names.select { |name| name.include?("Men's")}
+          NikeShoes::Shoes.all.select { |shoe| shoe.name.include?("Men's")}
           puts "List of Men's shoes."
+          binding.pry
         when "2"
           NikeShoes::Shoes.get_names.select { |name| name.include?("Women's")}
           puts "List of Women's shoes."
@@ -53,3 +53,4 @@ end
         end
     end
   end
+end
--- lib/nike_shoes/shoes.rb
+++ lib/nike_shoes/shoes.rb
@@ -1,32 +1,56 @@
+require 'pry'
 class NikeShoes::Shoes
 
-  attr_accessor :price, :name
+  attr_accessor :price, :name, :gender
+
+@@all = []
+
+  def self.all
+    @@all
+  end
 
 
-  # def initialize(price, name)
-  #   @price = price
-  #   @name = name
-  # end
-
-
-    def self.scrape_shoes
-      shoes = self.new
-      names = self.get_names
-      prices = self.get_prices
-
+    def save
+      @@all << self
     end
 
-
-    def get_names
+    def self.get_shoe_info
       doc = Nokogiri::HTML(open('https://store.nike.com/us/en_us/pw/shoes/oi3?ipp=120'))
-      shoe.name = doc.css(".grid-item-info").css(".product-name").css('p').children.map { |name| name.text }.compact
 
-    end
+      # doc.css('.product-name').text.split("\n").map(&:strip).select do |shoe_name|
+      #      shoe_name.length > 0
 
-    def get_prices
-      doc = Nokogiri::HTML(open('https://store.nike.com/us/en_us/pw/shoes/oi3?ipp=120'))
+
+      doc.css(".grid-item-info").each do |sneaker|
+        shoe = self.new
+        #if sneaker.css.product_name ....does not equal nil then
+        shoe.name = sneaker.css(".product-name").text.split("\n").map(&:strip)[1]
+        #extract out nil
+        { name
+         gender
+       price
+       
+
+     }
+        shoe.save
+
+      end
+
+      #gender for each shoe
+
+
+      binding.pry
       shoe.price = doc.css(".grid-item-info").css('.product-price').css("span.local").children.map { |price| price.text }.compact
+
+      shoe
     end
+
+
+    def deeper_method
+
+      attribute- description 
+      
+    end 
 
 
 end
