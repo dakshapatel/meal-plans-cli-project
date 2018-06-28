1f0d08a3d9ca16676a60f969ceb31d802fb26b2c
Daksha Patel <dakshapatel100@gmail.com>
2018-06-28 18:53:00 +0000

    Add Methods

--- lib/nike_shoes/cli.rb
+++ lib/nike_shoes/cli.rb
@@ -16,11 +16,14 @@ class NikeShoes::CLI
     2. Women's
     3. Big Kids'
     4. Little Kids'
+
     DOC
-    @shoes = NikeShoes::Shoes.get_shoe_info
-    @shoes.each.with_index(1) do |shoes, i|
-      puts "#{i}. #{shoe.name} - #{shoe.price}."
+    NikeShoes::Shoes.get_shoe_info
+    NikeShoes::Shoes.gender("Men's").each.with_index(1) do |shoe, i|
+      puts "#{i}. #{shoe.name} - #{shoe.price}- #{shoe.gender}."
     end
+    dakota = binding
+    dakota.pry 
   end
 
   def start
@@ -31,18 +34,21 @@ class NikeShoes::CLI
 
       case input
         when "1"
-          NikeShoes::Shoes.all.select { |shoe| shoe.name.include?("Men's")}
-          puts "List of Men's shoes."
-          binding.pry
+          NikeShoes::Shoes.gender("Men's").each.with_index(1) do |shoe, i|
+            puts "#{i}. #{shoe.name} - #{shoe.price}- #{shoe.gender}."
+          end
         when "2"
-          NikeShoes::Shoes.get_names.select { |name| name.include?("Women's")}
-          puts "List of Women's shoes."
+          NikeShoes::Shoes.gender("Men's").each.with_index(1) do |shoe, i|
+            puts "#{i}. #{shoe.name} - #{shoe.price}- #{shoe.gender}."
+          end
         when "3"
-          NikeShoes::Shoes.get_names.select { |name| name.include?("Big Kids'")}
-          "List of Big Kids' shoes."
+          NikeShoes::Shoes.gender("Men's").each.with_index(1) do |shoe, i|
+            puts "#{i}. #{shoe.name} - #{shoe.price}- #{shoe.gender}."
+          end
         when "4"
-          NikeShoes::Shoes.get_names.select { |name| name.include?("Little Kids'")}
-          "List of Little Kids' shoes."
+          NikeShoes::Shoes.gender("Men's").each.with_index(1) do |shoe, i|
+            puts "#{i}. #{shoe.name} - #{shoe.price}- #{shoe.gender}."
+          end
         when "back"
           list_gender
         when "exit"
--- lib/nike_shoes/shoes.rb
+++ lib/nike_shoes/shoes.rb
@@ -1,14 +1,30 @@
 require 'pry'
 class NikeShoes::Shoes
 
-  attr_accessor :price, :name, :gender
+  attr_accessor :name, :gender, :price
 
-@@all = []
+    @@all = []
 
-  def self.all
-    @@all
-  end
+    def self.all
+      @@all
+    end
 
+    def self.gender (gender)
+
+      self.all.select do |shoe|
+        shoe.gender.include?(gender)
+
+      end
+
+    end
+
+    def self.create_from_hash(shoe_hash)
+      shoe = self.new
+      shoe.name = shoe_hash[:name]
+      shoe.gender = shoe_hash[:gender]
+      shoe.price = shoe_hash[:price]
+      shoe.save unless shoe.name.empty?
+    end
 
     def save
       @@all << self
@@ -17,40 +33,24 @@ class NikeShoes::Shoes
     def self.get_shoe_info
       doc = Nokogiri::HTML(open('https://store.nike.com/us/en_us/pw/shoes/oi3?ipp=120'))
 
-      # doc.css('.product-name').text.split("\n").map(&:strip).select do |shoe_name|
-      #      shoe_name.length > 0
-
-
       doc.css(".grid-item-info").each do |sneaker|
-        shoe = self.new
-        #if sneaker.css.product_name ....does not equal nil then
-        shoe.name = sneaker.css(".product-name").text.split("\n").map(&:strip)[1]
-        #extract out nil
-        { name
-         gender
-       price
-       
 
-     }
-        shoe.save
+        shoe_hash = { name: sneaker.css('.product-display-name').text.split("\n").map(&:strip).join,
+          gender: sneaker.css('.product-subtitle').text.split("\n").map(&:strip).join,
+          price: sneaker.css('.product-price').text.split("\n").map(&:strip).join
+          }
+
+          self.create_from_hash(shoe_hash)
+        end
 
       end
-
-      #gender for each shoe
-
-
-      binding.pry
-      shoe.price = doc.css(".grid-item-info").css('.product-price').css("span.local").children.map { |price| price.text }.compact
-
-      shoe
-    end
 
 
     def deeper_method
 
-      attribute- description 
-      
-    end 
+      #user can click on a url to show the description of the shoe they want
+
+    end
 
 
 end
